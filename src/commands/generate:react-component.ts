afe558a9c6050be39f3b059fa12290e1bd46421f
thales-gelinger <thalesgelinger99@gmail.com>
2020-03-18 22:11:55 -0300

    feat: generate reactjs component

---
+++ src/commands/generate:react-component.ts
@@ -0,0 +1,48 @@
+import { GluegunToolbox } from 'gluegun'
+
+import toPascalCase from "../utils/toPascalCase"
+
+module.exports = {
+  name: 'generate:react-component',
+  description: 'Create a bootstrap backend project for node js',
+  run: async (toolbox: GluegunToolbox) => {
+    const {
+      print: { 
+        success, 
+        error, 
+        info
+      },
+      parameters,
+      template,
+      filesystem
+    } = toolbox
+
+    const name = parameters.first
+
+    if (!name) {
+      error('You must type the name of the component')
+      return
+    }
+
+    const nameCamelCase = toPascalCase(name)
+
+    const exportLine = `export { ${nameCamelCase} } from './${nameCamelCase}/${name}' \n`
+    
+    filesystem.append("src/components/index.js", exportLine)
+
+    info(`Creating component ${name}`)
+
+    await template.generate({
+      template: 'js/react-js/component/component.css',
+      target: `src/components/${name}/${name}.css`,
+    })
+
+    await template.generate({
+      template: 'js/react-js/component/component.js.ejs',
+      target: `src/components/${name}/${name}.js`,
+      props: { name, value: toPascalCase(name) }
+    })
+
+    success(`Generated ${name}`)
+  }
+}
