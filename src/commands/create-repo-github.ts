96904b7d42a79789101da0dcb516e6d630113f7a
thalesgelinger <thalesgelinger99@gmail.com>
2020-05-16 02:21:02 -0300

    feat: create repository on github

--- src/commands/create-repo-github.ts
+++ src/commands/create-repo-github.ts
@@ -6,7 +6,7 @@ module.exports = {
   description: 'Create a new repository on github',
   run: async (toolbox: GluegunToolbox) => {
     const {
-      print: { success, error, info, spin },
+      print: { success, error, spin },
       parameters,
       options,
       system,
@@ -17,7 +17,7 @@ module.exports = {
 
     const repoDescription = options?.d 
 
-    async function createRepo(name, description="") {
+    async function createRepo(name: String, description: String = "") {
 
         if(!name) {
             error("You must specify a name for your new project")
@@ -31,39 +31,52 @@ module.exports = {
             "private": false,
             "has_issues": true,
             "has_projects": true,
-            "has_wiki": true
+            "has_wiki": true,
+            "auto_init": true
         }
     
-        const { data: repository } = await githubApi.post("/user/repos", data, {
-                                auth: {
-                                    username: "thalesgelinger",
-                                    password: "Th@l3sG3l1ng3R"
-                                }
-                            })
+        async function getUserCredentials() {
+            
+            function getValueFromParentArrayItem(array: Array<String>, itemName: String): string {
+                return array[array.indexOf(itemName) + 1].toString()
+            }
+            
+            const gitconfig = await filesystem.readAsync(`${process.env.HOME}/.gitconfig`, "utf8")
+            
+            const gitconfigSanitized = gitconfig.split(/\]|\[/) 
+                
+            const userInfo = getValueFromParentArrayItem(gitconfigSanitized, "user")
+                                .replace(/\s/g, " ")
+                                .split(" ")
+                                .filter(item => item !== "" && item !== "=")
+        
+            return {
+                username: getValueFromParentArrayItem(userInfo,"name"),                        
+                password: getValueFromParentArrayItem(userInfo,"password")    
+            }
+        }
+
+        const auth = await getUserCredentials()
+
+        const { data: repository } = await githubApi.post("/user/repos", data, { auth })
     
         return repository.clone_url
     }
 
-    async function cloneRepo(repoUrl) {
-        system.run(`git clone ${repoUrl}`)
+    async function cloneRepo(repoUrl: String) {
+        await system.run(`git clone ${repoUrl}`)
     }
 
-    function doInitialCommit(repoName) {
-        system.run(`cd ${repoName}`)
-        filesystem.append("./README.md",`### ${repoName}`)
-        system.run("git add .")
-        system.run("git commit -m \"Initial commit\"")
-        system.run("git push")        
-    }
-
-    const repoUrl = createRepo(repoName, repoDescription)
-
-    console.log(repoUrl)
+    const spinner = spin("Creating repo")
+    const repoUrl = await createRepo(repoName, repoDescription)
+    spinner.stop()
 
     if(repoUrl) {
-        cloneRepo(repoUrl)
-        doInitialCommit(repoName)
-    }
+        spinner.start("Cloning remote repository to machine")
+        await cloneRepo(repoUrl)
+        spinner.stop()
+    }        
 
+    success("Repository succesfuly created")
   }
 }
